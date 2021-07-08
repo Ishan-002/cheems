a58c8687787c693050503e7517f7f792756950ef
Ishan-002 <ishan26rawat@gmail.com>
2021-07-08 17:13:08 +0530

    Add video calling + in-calling chat

--- server/routes/open-vidu/session.js
+++ server/routes/open-vidu/session.js
@@ -4,37 +4,6 @@ const axiosInstance = require('./axiosInstance');
 
 // TODO: Handle other http request codes also, when there's something else than 200
 
-// @route POST api/session
-// @desc Create a new OpenVidu session and return a connection token to the client
-// @access Protected
-router.post('/', (req, res) => {
-  axiosInstance
-    .post('/openvidu/api/sessions', {
-      headers: {
-        // TODO: Use customSessionId in headers to override the OpenVidu given sessionId
-      },
-    })
-    .then((response) => {
-      const session = response.data;
-      const sessionId = session.id;
-
-      return axiosInstance.post(
-        `/openvidu/api/sessions/${sessionId}/connection`,
-        // TODO: See openVidu roles and add roles for publisher and moderator according to the admin roles of user
-        {}
-      );
-    })
-    .then((response) => {
-      const connection = response.data;
-      const connectionToken = connection.token;
-      return res.status(200).json({ connectionToken: `${connectionToken}` });
-    })
-    .catch((error) => {
-      console.log(error);
-      return res.status(500).json({ error: 'Internal server error' });
-    });
-});
-
 // @route POST api/session/:sessionId
 // @desc Adds a new connection to an OpenVidu session
 // @access Protected
@@ -47,7 +16,7 @@ router.post('/:sessionId', (req, res) => {
       // TODO: See openVidu roles and add roles for publisher and moderator according to the admin roles of user
     )
     .then((response) => {
-      console.log(response.data);
+      // console.log(response.data);
       const connection = response.data;
       const connectionToken = connection.token;
       const connectionId = connection.connectionId;
@@ -57,7 +26,7 @@ router.post('/:sessionId', (req, res) => {
       });
     })
     .catch((error) => {
-      console.log(error);
+      // console.log(error);
       return res.status(500).json({ error: 'Internal server error' });
     });
 });
@@ -89,6 +58,33 @@ router.delete('/:sessionId/:connectionId', (req, res) => {
       return res.status(200);
     })
     .catch((error) => {
+      return res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
+// @route POST api/session
+// @desc Create a new OpenVidu session
+// @access Protected
+router.post('/', (req, res) => {
+  console.log(req.body.sessionId);
+  axiosInstance
+    .post('/openvidu/api/sessions', {
+      customSessionId: req.body.sessionId,
+
+      // TODO: Use customSessionId in headers to override the OpenVidu given sessionId
+    })
+    .then((response) => {
+      const session = response.data;
+      const sessionId = session.id;
+      return res.status(200).json({ sessionId: sessionId });
+    })
+    .catch((error) => {
+      console.log(error);
+      const response = error.response;
+      const sessionId = response.data.id;
+      if (response && response.status == 409) {
+        return res.status(200).json({ sessionId: sessionId });
+      }
       return res.status(500).json({ error: 'Internal server error' });
     });
 });
