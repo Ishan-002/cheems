3f435dcecedb542ce1800047c97556a19752efa7
Ishan-002 <ishan26rawat@gmail.com>
2021-07-10 01:00:28 +0530

    Fix styling for video call toolbar

---
+++ src/themes.js
@@ -0,0 +1,22 @@
+export const ThemeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#61afef',
+      light: '#cee6fa',
+      dark: '#376386',
+      contrastText: '#000000',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#282c34',
+      paper: '#383d48',
+    },
+    text: {
+      secondary: '#bfc4cf',
+      primary: '#abb2bf',
+    },
+  },
+};
